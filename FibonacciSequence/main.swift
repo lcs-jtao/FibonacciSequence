f3635b76b5693947f86534458284e063b735dbd6
Joyce Tao <jtao@lcs.on.ca>
2022-11-15 19:12:18 -0500

    implement Fibonacci sequence using recursion, and check if the result is the same as using iteration

--- FibonacciSequence/main.swift
+++ FibonacciSequence/main.swift
@@ -11,9 +11,27 @@ import Foundation
 var values = [0,1]
 var newValue = 0
 
-for i in 0...10 {
+for i in 0...15 {
     newValue = values[i] + values[i+1]
     values.append(newValue)
 }
 print(values)
 
+// recursive
+func fibonacciSequence(with i: inout Int, with j: inout Int) {
+    let k = i + j
+    print(k, terminator: " ")
+    i = j
+    j = k
+    if k <= 1000 {
+        fibonacciSequence(with: &i, with: &j)
+    } else {
+        print("That's the Fibonacci Sequence!")
+    }
+}
+
+var valueOne = 0
+var valueTwo = 1
+print(valueOne, terminator: " ")
+print(valueTwo, terminator: " ")
+fibonacciSequence(with: &valueOne, with: &valueTwo)
